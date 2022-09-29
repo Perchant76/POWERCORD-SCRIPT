ac4fdb561cf9ea0784319c8da23c72a95255a2ea
Perchant76 <63096336+Perchant76@users.noreply.github.com>
2022-09-29 18:45:02 +0200

    updated discord restart

--- install/install-win.bat
+++ install/install-win.bat
@@ -1,9 +1,8 @@
 cd %UserProfile%
+taskkill /f /im DiscordCanary.exe
+echo installing Powercord...
 git clone https://github.com/powercord-org/powercord
 cd "powercord"
 npm i
 npm run plug
-echo installation done...
-echo restarting discord...
-taskkill /f /im DiscordCanary.exe
 pause
